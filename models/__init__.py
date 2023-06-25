dbea8b1f2be59c0902fb2a85fc13e11876661889
Pinerealm <osalami09@gmail.com>
2023-06-26 00:15:32 +0300

    Refactor to remove repetition

--- models/__init__.py
+++ models/__init__.py
@@ -7,8 +7,8 @@ storage_type = os.getenv('HBNB_TYPE_STORAGE')
 if storage_type == 'db':
     from .engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from .engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
