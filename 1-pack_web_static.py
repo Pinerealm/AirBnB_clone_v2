9eeedf4a9cb7a298200a207d9dbe3475954db2ca
Pinerealm <osalami09@gmail.com>
2023-07-06 17:46:45 +0300

    Check if the archive exists at the end of runtime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,22 +2,23 @@
 """A Fabric script that generates a .tgz archive from the contents of
 AirBnB_clone_v2 web_static folder
 """
-from fabric.api import local, settings
+from datetime import datetime
+from fabric.api import local
+import os
 
 
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder
     of AirBnB_clone_v2
     """
-    try:
-        with settings(warn_only=True):
-            if local("test -d versions").failed:
-                local("mkdir versions")
-        date = local("date +%Y%m%d%H%M%S", capture=True)
-        file_name = local('echo "versions/web_static_{}.tgz"'
-                          .format(date), capture=True)
-        local("tar -cvzf {} web_static".format(file_name))
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
 
-        return file_name
-    except Exception:
-        return None
+    print("Packing web_static to {}".format(file_name))
+    local("tar -cvzf {} web_static".format(file_name))
+    file_size = os.stat(file_name).st_size
+    print("web_static packed: {} -> {}Bytes".format(file_name, file_size))
+
+    return file_name if os.path.exists(file_name) else None
