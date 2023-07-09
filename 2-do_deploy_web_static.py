e1304190847cb61aad18b44bc261980d575a4eeb
Pinerealm <osalami09@gmail.com>
2023-07-09 18:47:56 +0300

    Add the do_pack function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3
 """A Fabric script that distributes an archive to specified web servers
 """
-from fabric.api import env, put, run
+from datetime import datetime
+from fabric.api import env, put, run, local, settings
 import os
 
 
-# env.hosts = ["100.25.167.113", "54.237.105.147"]
-# env.user = "ubuntu"
-# env.key_filename = "~/.ssh/id_rsa"
+env.hosts = ["100.25.167.113", "54.237.105.147"]
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
+
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder
+    of AirBnB_clone_v2
+
+    Returns:
+        str: Path to the archive created
+    """
+    if not os.path.isdir("versions"):
+        os.makedirs("versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(date)
+
+    print("Packing web_static to {}".format(file_name))
+    with settings(warn_only=True):
+        result = local("tar -cvzf {} web_static".format(file_name))
+        if result.failed:
+            return None
+    file_size = os.stat(file_name).st_size
+    print("web_static packed: {} -> {}Bytes".format(file_name, file_size))
+
+    return file_name
 
 
 def do_deploy(archive_path):
