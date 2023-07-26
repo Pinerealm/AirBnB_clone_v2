37944961f7ebe1e8c4d858daee000db9533fa9a2
Pinerealm <osalami09@gmail.com>
2023-07-26 17:59:55 +0300

    Refactor the do_create method

--- console.py
+++ console.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-""" Console Module """
+"""The console/command interpreter for the AirBnB_clone project
+"""
 import cmd
-import sys
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
 from models.user import User
+
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+
 from models.review import Review
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
-    """ Contains the functionality for the HBNB console"""
+    """Defines the HBNBCommand class for AirBnB_clone
 
-    # determines prompt for interactive/non-interactive modes
+    Attributes:
+        prompt (str): The command prompt
+        classes (tuple): The list of classes that can be created
+    """
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
-
     classes = {
                'BaseModel': BaseModel, 'User': User, 'Place': Place,
                'State': State, 'City': City, 'Amenity': Amenity,
@@ -31,78 +36,115 @@ class HBNBCommand(cmd.Cmd):
             }
 
     def precmd(self, line):
-        """Reformat command line for advanced command syntax.
+        """Reformats the command line for the advanced command syntax.
 
         Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
         (Brackets denote optional fields in usage example.)
         """
         if not sys.__stdin__.isatty():
             print('(hbnb) ', end='')
-        _cmd = _cls = _id = _args = ''  # initialize line elements
+        return line
+    #     _cmd = _cls = _id = _args = ''  # initialize line elements
 
-        # scan for general formating - i.e '.', '(', ')'
-        if not ('.' in line and '(' in line and ')' in line):
-            return line
+    #     # scan for general formating - i.e '.', '(', ')'
+    #     if not ('.' in line and '(' in line and ')' in line):
+    #         return line
 
-        try:  # parse line left to right
-            pline = line[:]  # parsed line
+    #     try:  # parse line left to right
+    #         pline = line[:]  # parsed line
 
-            # isolate <class name>
-            _cls = pline[:pline.find('.')]
+    #         # isolate <class name>
+    #         _cls = pline[:pline.find('.')]
 
-            # isolate and validate <command>
-            _cmd = pline[pline.find('.') + 1:pline.find('(')]
-            if _cmd not in HBNBCommand.dot_cmds:
-                raise Exception
+    #         # isolate and validate <command>
+    #         _cmd = pline[pline.find('.') + 1:pline.find('(')]
+    #         if _cmd not in HBNBCommand.dot_cmds:
+    #             raise Exception
 
-            # if parentheses contain arguments, parse them
-            pline = pline[pline.find('(') + 1:pline.find(')')]
-            if pline:
-                # partition args: (<id>, [<delim>], [<*args>])
-                pline = pline.partition(', ')  # pline convert to tuple
+    #         # if parentheses contain arguments, parse them
+    #         pline = pline[pline.find('(') + 1:pline.find(')')]
+    #         if pline:
+    #             # partition args: (<id>, [<delim>], [<*args>])
+    #             pline = pline.partition(', ')  # pline convert to tuple
 
-                # isolate _id, stripping quotes
-                _id = pline[0].replace('\"', '')
-                # possible bug here:
-                # empty quotes register as empty _id when replaced
+    #             # isolate _id, stripping quotes
+    #             _id = pline[0].replace('\"', '')
+    #             # possible bug here:
+    #             # empty quotes register as empty _id when replaced
 
-                # if arguments exist beyond _id
-                pline = pline[2].strip()  # pline is now str
-                if pline:
-                    # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] == '}'\
-                            and type(eval(pline)) is dict:
-                        _args = pline
-                    else:
-                        _args = pline.replace(',', '')
-                        # _args = _args.replace('\"', '')
-            line = ' '.join([_cmd, _cls, _id, _args])
+    #             # if arguments exist beyond _id
+    #             pline = pline[2].strip()  # pline is now str
+    #             if pline:
+    #                 # check for *args or **kwargs
+    #                 if pline[0] == '{' and pline[-1] == '}'\
+    #                         and type(eval(pline)) is dict:
+    #                     _args = pline
+    #                 else:
+    #                     _args = pline.replace(',', '')
+    #                     # _args = _args.replace('\"', '')
+    #         line = ' '.join([_cmd, _cls, _id, _args])
 
-        except Exception as mess:
-            pass
-        finally:
-            return line
+    #     except Exception as mess:
+    #         pass
+    #     finally:
+    #         return line
 
-    def do_quit(self, command):
-        """ Method to exit the HBNB console"""
+    def parseline(self, line):
+        """Reformats the command line for the advanced command syntax.
+        """
+        if '(' in line and ')' in line:
+            line = line.replace('.', ' ', 1).replace('(', ' ', 1)
+            line = line.replace(')', '')
+            if '{' in line and ': ' in line and '}' in line:
+                #  Update if dictionary is given
+                pre_dict, dict_info = line.split(', {', 1)
+                pre_dict = pre_dict.split(" ")
+                pre_dict[2] = pre_dict[2].replace('"', '')
+                line = ' '.join([pre_dict[1], pre_dict[0], pre_dict[2],
+                                 '{' + dict_info])
+                return super().parseline(line)
+
+            line = line.split()
+            if len(line) == 3:
+                #  For show and destroy
+                line[2] = line[2].replace('"', '')
+                line = " ".join([line[1], line[0], line[2]])
+            elif len(line) >= 5:
+                #  Update if attribute name and value are given
+                line[2] = line[2].replace('"', '').replace(',', '')
+                line[3] = line[3].replace('"', '').replace(',', '')
+                line[4] = line[4].replace('"', '')
+                line = " ".join([line[1], line[0], line[2], line[3], line[4]])
+            else:
+                line = " ".join([line[1], line[0]])
+
+        return super().parseline(line)
+
+    def do_quit(self, arg):
+        """Exit the program with the command 'quit'
+        """
         return True
 
     def help_quit(self):
-        """ Prints the help documentation for quit  """
-        print("Exits the program with formatting\n")
+        """Documentation for the quit command
+        """
+        print("Exits the program")
 
     def do_EOF(self, arg):
-        """ Handles EOF to exit program """
+        """Exit the program on receiving the EOF signal
+        """
         print()
         return True
 
     def help_EOF(self):
-        """ Prints the help documentation for EOF """
-        print("Exits the program without formatting\n")
+        """Documentation for the EOF signal
+        """
+        print("Exits the program on receiving the EOF signal")
 
     def emptyline(self):
-        """ Overrides the emptyline method of CMD """
-        pass
+        """Do nothing on receiving an empty line
+        """
+        return False
 
     def do_create(self, arg):
         """Create an object of any class with optional parameters
@@ -119,26 +161,30 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         tokens = arg.split()
-        if tokens[0] not in HBNBCommand.classes:
+        if tokens[0] not in self.classes:
             print("** class doesn't exist **")
             return
 
-        new_instance = HBNBCommand.classes[tokens[0]]()
+        new_instance = self.classes[tokens[0]]()
         if len(tokens) > 1:
             for i in range(1, len(tokens)):
                 if "=" in tokens[i]:
                     key, value = tokens[i].split("=")
-                    value = value.replace("_", " ").replace('"', '')
-                    if key in HBNBCommand.types:
-                        value = HBNBCommand.types[key](value)
+                    value = value.replace("_", " ").replace('\"', '"')
+                    if value[0] == '"' and value[-1] == '"':
+                        value = value[1:-1]
+                    if key in self.types:
+                        value = self.types[key](value)
                     setattr(new_instance, key, value)
         print(new_instance.id)
         new_instance.save()
 
     def help_create(self):
-        """ Help information for the create method """
-        print("Creates a class of any type")
-        print("[Usage]: create <className>\n")
+        """Documentation for the create command"""
+        print("Creates a class instance with optional parameters")
+        print("[Usage]: create <className> OR")
+        print("         create <className> <param1> <param2> <param3> ...")
+        print("Param syntax: <key name>=<value>\n")
 
     def do_show(self, args):
         """ Method to show an individual object """
@@ -216,11 +262,11 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
                 return
             for value in storage.all(self.classes[cls]).values():
-                del value.__dict__['_sa_instance_state']
+                # del value.__dict__['_sa_instance_state']
                 print_list.append(value)
         else:
             for value in storage.all().values():
-                del value.__dict__['_sa_instance_state']
+                # del value.__dict__['_sa_instance_state']
                 print_list.append(value)
 
         print('[{}]'.format(', '.join(str(x) for x in print_list)))
