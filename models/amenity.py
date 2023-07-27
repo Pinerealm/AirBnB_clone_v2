a48fd6c97222c88507783b61ad80eaef8530b751
Pinerealm <osalami09@gmail.com>
2023-07-27 16:39:41 +0300

    Refactor the documentation

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""The amenity module
+"""
 from .base_model import BaseModel, Base
-from models import storage_type, storage
+from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """The Amenity class
+    """Defines the Amenity class, inherits from BaseModel
+
+    Attributes:
+        name (str): The name of the amenity
+        place_amenities (list): Represents the Place-Amenity relationship
     """
     if storage_type == 'db':
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary="place_amenity",
                                        back_populates="amenities")
-        
+
     else:
         name = ""
