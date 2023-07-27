fbcaa9b077d3669a3e1cedc5d8d5a892a6e5891e
Pinerealm <osalami09@gmail.com>
2023-07-27 16:41:29 +0300

    Bring back the amenity_ids attribute

--- models/place.py
+++ models/place.py
@@ -70,6 +70,7 @@ class Place(BaseModel, Base):
         price_by_night = 0
         latitude = 0.0
         longitude = 0.0
+        amenity_ids = []
 
         @property
         def reviews(self):
