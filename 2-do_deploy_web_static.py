a41a4a0db0079e1a4c65d35f2439f1bb086c57be
Pinerealm <osalami09@gmail.com>
2023-07-30 01:04:13 +0300

    Comment out some env variables

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,8 @@ from fabric.api import env, put, run
 import os
 
 env.hosts = ["100.25.167.113", "54.237.105.147"]
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/id_rsa"
+# env.user = "ubuntu"
+# env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
